3a60faa5e84154adf7e66516225f7a080bd57d1e
FlutLab.io <>
2022-04-06 15:42:19 +0000

    Autosave

--- lib/nav-drawer.dart
+++ lib/nav-drawer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class DrawerWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -7,22 +8,10 @@ class DrawerWidget extends StatelessWidget {
         padding: EdgeInsets.zero,
         children: <Widget>[
           _drawerHeader(),
-          _drawerItem(
-              icon: Icons.audiotrack,
-              text: 'song',
-              onTap: () => print('Tap My Files')),
-          _drawerItem(
-              icon: Icons.assignment_sharp,
-              text: 'Album',
-              onTap: () => print('Tap Shared menu')),
-          _drawerItem(
-              icon: Icons.offline_bolt,
-              text: 'Offline',
-              onTap: () => print('Tap Recent menu')),
-          _drawerItem(
-              icon: Icons.delete,
-              text: 'Trash',
-              onTap: () => print('Tap Trash menu')),
+          _drawerItem(icon: Icons.audiotrack, text: 'song', onTap: () => print('Tap My Files')),
+          _drawerItem(icon: Icons.assignment_sharp, text: 'Album', onTap: () => print('Tap Shared menu')),
+          _drawerItem(icon: Icons.offline_bolt, text: 'Offline', onTap: () => print('Tap Recent menu')),
+          _drawerItem(icon: Icons.delete, text: 'Trash', onTap: () => print('Tap Trash menu')),
           Divider(height: 25, thickness: 1),
           Padding(
             padding: const EdgeInsets.only(left: 20.0, top: 10, bottom: 10),
@@ -32,35 +21,31 @@ class DrawerWidget extends StatelessWidget {
                   color: Colors.black54,
                 )),
           ),
-          _drawerItem(
-              icon: Icons.bookmark,
-              text: 'Family',
-              onTap: () => print('Tap Family menu')),
+          _drawerItem(icon: Icons.bookmark, text: 'Family', onTap: () => print('Tap Family menu')),
         ],
       ),
     );
   }
 }
+
 Widget _drawerHeader() {
   return UserAccountsDrawerHeader(
     currentAccountPicture: ClipOval(
-      child: Image(
-          image: AssetImage('assets/images/gambar.jpg'), fit: BoxFit.cover),
+      child: Image(image: AssetImage('assets/images/gambar.jpg'), fit: BoxFit.cover),
     ),
     otherAccountsPictures: [
       ClipOval(
-        child: Image(
-            image: AssetImage('assets/images/gambar.jpg'), fit: BoxFit.cover),
+        child: Image(image: AssetImage('assets/images/gambar.jpg'), fit: BoxFit.cover),
       ),
       ClipOval(
-        child: Image(
-            image: AssetImage('assets/images/gambar.jpg'), fit: BoxFit.cover),
+        child: Image(image: AssetImage('assets/images/gambar.jpg'), fit: BoxFit.cover),
       )
     ],
     accountName: Text('Belajar Flutter'),
     accountEmail: Text('hallo@belajarflutter.com'),
   );
 }
+
 Widget _drawerItem({IconData icon, String text, GestureTapCallback onTap}) {
   return ListTile(
     title: Row(
@@ -79,42 +64,4 @@ Widget _drawerItem({IconData icon, String text, GestureTapCallback onTap}) {
     ),
     onTap: onTap,
   );
-}
-
-import 'package:flutter/material.dart';
-
-class songs extends StatelessWidget {
-  final List lagu = [
-    "EXO - Monster ",
-    "Wayv - Take Off ",
-    "NCT2021 - Beautiful ",
-    "NCT 127 - Favorite ",
-    "NCT DREAM - BOOM ",
-    "Enhypen - Fever ",
-    "EXO - The Eve ",
-    "EXO - Lotto ",
-    "TXT - Anti Romantic ",
-    "Enhypen - Polaroid Love ",
-    "Treasure - Darari ",
-  ];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          return Card(
-            child: Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Text(lagu[index],
-                  style: TextStyle(
-                    fontSize: 25,
-                    color: Colors.black,
-                  )),
-            ),
-          );
-        },
-        itemCount: lagu.length,
-      ),
-    );
-  }
 }
