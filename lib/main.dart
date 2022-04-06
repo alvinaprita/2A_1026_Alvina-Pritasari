6c04a23725ea4b6fd3f7353205201b51847ccca6
FlutLab.io <>
2022-04-06 15:14:44 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,90 +1,23 @@
 import 'package:flutter/material.dart';
+import './nav-drawer.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    title: "BelajarFlutter.com",
+    home: BelajarNavigationDrawer(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: BelajarAppBar(),
-    );
-  }
-}
-
-class BelajarAppBar extends StatelessWidget {
+class BelajarNavigationDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: DefaultTabController(
-        length: 2,
-        child: NestedScrollView(
-          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-            return <Widget>[
-              SliverAppBar(
-                expandedHeight: 200.0,
-                floating: false,
-                pinned: true,
-                flexibleSpace: FlexibleSpaceBar(
-                  centerTitle: true,
-                  title: Text("Belajar SliverAppBar",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20.0,
-                      )),
-                  background: Image(
-                    image: AssetImage('assets/images/gambar.jpg'),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-              SliverPersistentHeader(
-                pinned: true,
-                delegate: _SliverAppBarDelegate(
-                  TabBar(
-                    labelColor: Colors.black87,
-                    unselectedLabelColor: Colors.grey,
-                    tabs: [
-                      new Tab(icon: new Icon(Icons.audiotrack), text: "Songs"),
-                      new Tab(icon: new Icon(Icons.collections), text: "Gallery"),
-                      new Tab(icon: new Icon(Icons.collections), text: "Album"),
-                    ],
-                  ),
-                ),
-              ),
-            ];
-          },
-          
-          ),
-        ),
+      appBar: AppBar(
+        title: Text("Belajar Drawer Nav"),
       ),
+      drawer: DrawerWidget(),
+      endDrawer: DrawerWidget(),
+      body: Center(child: Text('Belajar Navigation Drawer di Flutter', style: TextStyle(fontSize: 20))),
     );
-  }
-}
-
-class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
-  _SliverAppBarDelegate(this._tabBar);
-
-  final TabBar _tabBar;
-
-  @override
-  double get minExtent => _tabBar.preferredSize.height;
-  @override
-  double get maxExtent => _tabBar.preferredSize.height;
-  @override
-  double get maxExtent => _tabBar.preferredSize.height;
-
-  @override
-  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return new Container(
-      child: _tabBar,
-    );
-  }
-
-  @override
-  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
-    return false;
   }
 }
