f66a2dd4e7b4dde96663e3d79463aae04761e0de
FlutLab.io <>
2022-04-07 14:24:23 +0000

    Autosave

--- lib/routes.dart
+++ lib/routes.dart
@@ -7,6 +7,8 @@ class RouteGenerator {
     // final args = settings.arguments;
 
     switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => HomePage());
       case '/about':
         return MaterialPageRoute(builder: (_) => About());
       case '/porto':
@@ -17,5 +19,14 @@ class RouteGenerator {
       default:
         return _errorRoute();
     }
+  }
+
+  static Route<dynamic> _errorRoute() {
+    return MaterialPageRoute(builder: (_) {
+      return Scaffold(
+        appBar: AppBar(title: Text("Error")),
+        body: Center(child: Text('Error page')),
+      );
+    });
   }
 }
