e83f4e61dd00adab637402f91915cecf27c8af18
FlutLab.io <>
2022-04-14 13:39:50 +0000

    Autosave

--- lib/signup_page.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class SignupPage extends StatefulWidget {
-  static String tag = 'signup-page';
-  @override
-  _SignupPageState createState() => new _SignupPageState();
-}
