1262f43fadf69b57866db48a0f38ce4543c2b661
FlutLab.io <>
2022-04-14 06:19:58 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -2,52 +2,113 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: 'belajar form',
+    title: "Belajar Form ",
     home: BelajarForm(),
   ));
 }
 
 class BelajarForm extends StatefulWidget {
-  const BelajarForm({Key key}) : super(key: key);
-
   @override
   _BelajarFormState createState() => _BelajarFormState();
 }
 
 class _BelajarFormState extends State<BelajarForm> {
-  final _formkey = GlobalKey<FormState>();
+  final _formKey = GlobalKey<FormState>();
+  double nilaiSlider = 1;
+  bool nilaiCheckBox = false;
+  bool nilaiSwitch = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Belajat Form'),
+        title: Text("Belajar form"),
       ),
       body: Form(
-        key: _formkey,
-        child: Container(
-          padding: EdgeInsets.all(20.0),
-          child: Column(
-            children: [
-              TextFormField(
-                keyboardType: TextInputType.phone,
-                decoration: InputDecoration(hintText: "contoh: 081xxxxxxxxx", labelText: "Nomor Telp", icon: Icon(Icons.phone), border: OutlineInputBorder(borderRadius: BorderRadius.circular(5.0))),
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'No Telp kosong';
-                  }
-                  return null;
-                },
-              ),
-              RaisedButton(
+        key: _formKey,
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: new InputDecoration(
+                      hintText: "contoh: agus",
+                      labelText: "Nama Lengkap",
+                      icon: Icon(Icons.people),
+                      border: OutlineInputBorder(borderRadius: new BorderRadius.circular(5.0)),
+                    ),
+                    validator: (String value) {
+                      if (value.isEmpty) {
+                        return 'Nama tidak boleh kosong';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    obscureText: true,
+                    decoration: new InputDecoration(
+                      labelText: "Password",
+                      icon: Icon(Icons.security),
+                      border: OutlineInputBorder(borderRadius: new BorderRadius.circular(5.0)),
+                    ),
+                    validator: (String value) {
+                      if (value.isEmpty) {
+                        return 'Password tidak boleh kosong';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                CheckboxListTile(
+                  title: Text('Belajar Dasar Flutter'),
+                  subtitle: Text('Dart, widget, http'),
+                  value: nilaiCheckBox,
+                  activeColor: Colors.deepPurpleAccent,
+                  onChanged: (value) {
+                    setState(() {
+                      nilaiCheckBox = value;
+                    });
+                  },
+                ),
+                SwitchListTile(
+                  title: Text('Backend Programming'),
+                  subtitle: Text('Dart, Nodejs, PHP, Java, dll'),
+                  value: nilaiSwitch,
+                  activeTrackColor: Colors.pink[100],
+                  activeColor: Colors.red,
+                  onChanged: (value) {
+                    setState(() {
+                      nilaiSwitch = value;
+                    });
+                  },
+                ),
+                Slider(
+                  value: nilaiSlider,
+                  min: 0,
+                  max: 100,
+                  onChanged: (value) {
+                    setState(() {
+                      nilaiSlider = value;
+                    });
+                  },
+                ),
+                RaisedButton(
                   child: Text(
-                    'Submit',
+                    "Submit",
                     style: TextStyle(color: Colors.white),
                   ),
                   color: Colors.blue,
                   onPressed: () {
-                    if (_formkey.currentState.validate()) {}
-                  })
-            ],
+                    if (_formKey.currentState.validate()) {}
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
